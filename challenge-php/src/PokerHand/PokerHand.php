4c4e96841636d9813d7f26579001d76511c7c747
ellytea <nallelyt4@gmail.com>
2020-04-21 19:13:49 -0600

    Refactored, pased tests, and Aces go low

--- challenge-php/src/PokerHand/PokerHand.php
+++ challenge-php/src/PokerHand/PokerHand.php
@@ -6,77 +6,65 @@ class PokerHand {
 
     private $hand;
     private $ranks = ['2','3','4','5','6','7','8','9','T','J','Q','K','A'];
-    private $suits = ['s', 'c', 'd', 'h'];
+    private $suits = ['s','c','d','h'];
     private $rank_cards;
     private $suit_cards;
-    private $is_paired;
+    private $result = 'High Card';
 
-    //passes a string of cards //ex. ['As Ks Qs Js 10s']
-    public function __construct($hand) {
-      //Replaces '10' with 'T' for easy char count
-      $noTen = str_replace('10', 'T', $hand);
-      $this->hand = str_replace(' ', '', $noTen);
-
-      //creates array with card rank as key and the number of times it;s in the array //ex. ['A'=>2, '4'=>1, '6'=>1, '10'=>1]
-      foreach(count_chars($this->hand, 1) as $char => $val) {
+    //passes a string of cards //ex. 'As Ks Qs Js 10s'
+    public function __construct($str) {
+      $t_hand = str_replace('10', 'T', $str);
+      $str_hand = str_replace(' ', '', $t_hand);
+      $hand = explode(' ', $t_hand);
+      //creates key value array //ex.['A'=>2, '4'=>1, '6'=>1, '10'=>1]
+      foreach(count_chars($str_hand, 1) as $char => $val) {
         in_array(chr($char),$this->ranks) ? $this->rank_cards[chr($char)] = $val :$this->suit_cards[chr($char)] = $val;
-      } 
+      }
+      //creates rank value array stored in $hand
+      $keys = array_keys($this->rank_cards);
+      foreach($hand as $el){
+        $trim = rtrim($el, $el[1]);
+        if ($trim == 'A' && array_sum($keys) == 14){
+          $this->hand[] = 1; continue;
+        }
+        $this->hand[] = array_search($trim, $this->ranks)+2;
+      }
     }
 
-    //pny checks royal function if hand values array length is 5.. Need more logic here
     private function check_pairs(){
       $faceCount = array_values($this->rank_cards);
       sort($faceCount);
-      $faceCount == [1, 1, 1, 2]? $this->is_paired = 'One Pair': null;
-      $faceCount == [1, 2, 2]? $this->is_paired = 'Two Pairs': null;
-      $faceCount == [1, 1, 3]? $this->is_paired = 'Three of a Kind': null;
-      $faceCount == [1, 4]? $this->is_paired = 'Four of a Kind': null;
-      $faceCount == [2, 3]? $this->is_paired = 'Full House': null;
-      $faceCount == [1,1,1,1,1]?$this->check_royal() :null;
+      $faceCount == [1,1,1,1,1]? $this->check_royal(): null;
+      $faceCount == [1, 1, 1, 2]? $this->result = 'One Pair': null;
+      $faceCount == [1, 2, 2]? $this->result = 'Two Pairs': null;
+      $faceCount == [1, 1, 3]? $this->result = 'Three of a Kind': null;
+      $faceCount == [1, 4]? $this->result = 'Four of a Kind': null;
+      $faceCount == [2, 3]? $this->result = 'Full House': null;
     }
 
     private function check_flush(){
-      return array_sum($this->suit_cards) == 5 ? true:false;
+      return count($this->suit_cards) === 1? true: false;
     }
 
     private function check_royal(){
-      $card_keys = array_keys($this->rank_cards);
-      if (preg_match("/[TJQKA]/", $card_keys[0]) === 1){
-        return true;
-      } else {
-        $this->check_straight($card_keys);
-      } 
+      array_sum($this->hand) === 60 && $this->check_flush()? $this->result = 'Royal Flush': $this->check_straight();
     }
 
-    // need to rewrite to use index position as value
     private function check_straight(){
-      $keys = array_keys($this->rank_cards);
-      if ($keys[0]+1 == $keys[1] && $keys[1]+1 == $keys[2]){
-         return true;
-      }
+      sort($this->hand);
+     if ($this->hand[2]+1 == $this->hand[3] && $this->hand[2]-1 == $this->hand[1] && $this->check_flush()){
+        $this->result = 'Straight Flush';
+     } else if ($this->hand[2]+1 == $this->hand[3] && $this->hand[2]-1 == $this->hand[1]){
+       $this->result = 'Straight';
+     }
     }
     
-    // need to refactor
-    public function rank_hand(){
+    public function get_rank(){
       $this->check_pairs();
-      if ($this->check_royal() && $this->check_flush()){
-        return "Royal Flush";
+      if ($this->check_flush() && $this->result == 'High Card') {
+        $this->result = 'Flush';
       }
-      if($this->check_royal() && !$this->check_flush()){
-        return "Straight Flush";
-      }
-      if($this->is_paired){
-        return $this->is_paired;
-      }
-      if($this->check_straight()){
-        return "Straight";
-      }
-      if ($this->check_flush()){
-        return "Flush";
-      }
-      else {
-        return "High Card";
-      }
+     return $this->result;
     }
 }
 
